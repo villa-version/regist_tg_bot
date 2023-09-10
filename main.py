6672a484963a04befe99f372b2b262b53abf529e
villa-version <volinetsilia@gmail.com>
2023-09-10 22:12:17 +0300

    add db. There are contain login and password of user. There are bugs

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import telebot as tg
+import sqlite3
+import json
 
 
 TOKEN = '6532152577:AAFqcSpzFShc2rIYrQUb6zaKxEUqCDHeLls'
 bot = tg.TeleBot(TOKEN)
 reg_allow = False
+
+
+#db = sqlite3.connect('users_data.db')
+#cur = db.cursor()
+#cur.execute("""CREATE TABLE users (
+#    login text,
+#    password text
+#)""")
+#db.commit()
+#db.close()
 
 
 @bot.message_handler(commands=['start'])
@@ -13,18 +25,42 @@ def start(message):
     markup.add(tg.types.InlineKeyboardButton('Нет', callback_data='negative'))
     bot_text = 'Здравствуйте {name}. Вы уже зарегистрировались здесь?'.format(name=message.from_user.first_name)
     bot.send_message(message.chat.id, bot_text, reply_markup=markup)
+    db = sqlite3.connect('users_data.db')
+    cur = db.cursor()
+    cur.execute('SELECT * FROM users')
+    text = cur.fetchall()
+    cur.close()
+    db.close()
+    print(text)
+    # bot.send_message(message.chat.id, json.load(text))
 
 
 @bot.message_handler()
 def register(message):
     global reg_allow
     if reg_allow:
-        try:
-            login, password = message.text.split(' ')
-            bot.send_message(message.chat.id, f'Ваши данные\n Login: {login}\n Password: {password}')
-            reg_allow = False
-        except Exception:
-            bot.send_message(message.chat.id, 'Что то пошло не так! Введите еще раз!')
+        #try:
+        login, password = message.text.split(' ')
+        db = sqlite3.connect('users_data.db')
+        cur = db.cursor()
+        sql = 'INSERT INTO users (login, password) VALUES (?, ?)'
+        cur.execute(sql, (login, password))
+        cur.close()
+        db.close()
+        reg_allow = False
+        #except Exception:
+        #    bot.send_message(message.chat.id, f'Что то пошло не так! Введите еще раз!')
+
+
+@bot.message_handler(commands=['db'])
+def check_db(message):
+    db = sqlite3.connect('users_data.db')
+    cur = db.cursor()
+    cur.execute('SELECT * FROM users')
+    text = cur.fetchall()
+    cur.close()
+    db.close()
+    bot.send_message(message.chat.id, json.load(text))
 
 
 @bot.callback_query_handler(func=lambda callback: True)
@@ -38,4 +74,5 @@ def check_callback(callback):
 
 
 bot.infinity_polling()
+
 
