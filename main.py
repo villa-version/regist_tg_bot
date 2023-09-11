13c1ba705f2fc4f3b055c276d8368b6268974efc
villa-version <volinetsilia@gmail.com>
2023-09-11 23:48:37 +0300

    add func clear_db add log in login and register are in one func now

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import telebot as tg
 import sqlite3
-from pprint import pprint
-import json
 
 
 TOKEN = '6532152577:AAFqcSpzFShc2rIYrQUb6zaKxEUqCDHeLls'
 bot = tg.TeleBot(TOKEN)
 reg_allow = False
+log_allow = False
 
 
 #db = sqlite3.connect('users_data.db')
@@ -28,24 +27,45 @@ def start(message):
     bot.send_message(message.chat.id, bot_text, reply_markup=markup)
 
 
+@bot.message_handler(commands=['cleardb'])
+def clear_db(message):
+    try:
+        _, login, password = message.text.split(' ')
+        req = f'DELETE FROM users WHERE login="{login}" AND password="{password}"'
+    except ValueError:
+        req = 'DELETE FROM users'
+    db = sqlite3.connect('users_data.db')
+    cur = db.cursor()
+    cur.execute(req)
+    db.commit()
+    cur.close()
+    db.close()
+
+
 @bot.message_handler(commands=['db'])
 def check_db(message):
     db = sqlite3.connect('users_data.db')
     cur = db.cursor()
     cur.execute('SELECT * FROM users')
     text = cur.fetchall()
-    pprint(text)
     cur.close()
     db.close()
-    #bot.send_message(message.chat.id, json.load(text))
+    msg = 'LIST OF USERS DATA\n'
+    for ud in text:
+        msg += '{log}: {psswrd}\n'.format(log=ud[0], psswrd=ud[1])
+    bot.send_message(message.chat.id, msg)
 
 
 @bot.message_handler()
-def register(message):
-    global reg_allow
+def account_operations(message):
+    global reg_allow, log_allow
     if reg_allow:
-        #try:
-        login, password = message.text.split(' ')
+        try:
+            login, password = message.text.split(' ')
+            reg_allow = False
+        except ValueError:
+            bot.send_message(message.chat.id, f'Вы ввели не верно! Введите еще раз! (Через пробел)')
+            return
         db = sqlite3.connect('users_data.db')
         cur = db.cursor()
         req = f"INSERT INTO users(login, password) VALUES('{login}','{password}')"
@@ -53,15 +73,35 @@ def register(message):
         db.commit()
         cur.close()
         db.close()
-        reg_allow = False
-        #except Exception:
-        #    bot.send_message(message.chat.id, f'Что то пошло не так! Введите еще раз!')
+        bot.send_message(message.chat.id, 'Поздравляю! Вы вошли в систему успешно!')
+    elif log_allow:
+        try:
+            login, password = message.text.split(' ')
+        except ValueError:
+            bot.send_message(message.chat.id, f'Вы ввели не верно! Введите еще раз! (Через пробел)')
+            return
+        db = sqlite3.connect('users_data.db')
+        cur = db.cursor()
+        cur.execute('SELECT * FROM users')
+        text = cur.fetchall()
+        db.commit()
+        cur.close()
+        db.close()
+        for ud in text:
+            if ud[0] == login and ud[1] == password:
+                log_allow = False
+                break
+            if text.index(ud) == len(text)-1:
+                bot.send_message(message.chat.id, 'Не удалось войти в систему. Вы ввели не верный Логин или Пароль. Повторите попытку')
+                return
+        bot.send_message(message.chat.id, 'Поздравляю! Вы вошли в систему успешно!')
 
 
 @bot.callback_query_handler(func=lambda callback: True)
 def check_callback(callback):
-    global reg_allow
+    global reg_allow, log_allow
     if callback.data == 'positive':
+        log_allow = True
         bot.send_message(callback.message.chat.id, 'Давайте залогинемся! Введите Пожалуйста ваш логин и пароль через пробел.')
     elif callback.data == 'negative':
         reg_allow = True
